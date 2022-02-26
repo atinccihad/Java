40bed671794f610ffe5d8a78a99f56b7641ecfc1
Cihad Atınç <atinccihad@gmail.com>
2022-02-27 01:04:52 +0300

    Q

---
+++ Scanner/IncrementDecrement_05.java
@@ -0,0 +1,35 @@
+package day03_scanner;
+
+public class C05_IncrementDecrement {
+
+	public static void main(String[] args) {
+		
+		//atama islemi olursa sayinin degeri kalici olarak degisir
+		//atama islemi olmadan yapilan toplama cikarma vs islemler sadece o satirda yapilir
+		//sayinin degerini kalici olarak degistirmez
+		
+		int sayi1=10;
+		
+		System.out.println(sayi1+=5);  //15
+		//bu islem sayi1'in degerini 5 artirip sayi1'e assign ediyor
+		//dolayisiyla sayi1'in degeri kalici olarak degisiyor
+        System.out.println(sayi1);     //15
+        
+        System.out.println(sayi1+12);   //27
+        
+        System.out.println(sayi1);      //15  
+        
+        sayi1++;                 //her ne kadar esittir isareti(=) gorulmesede sayi1++ isleminde assign vardir
+                                 //dolayisiyla deger kalici olarak degisir
+        
+        System.out.println(sayi1);      //16
+        
+        
+        sayi1++;
+        System.out.println(sayi1);
+        
+        
+        
+	}
+
+}
