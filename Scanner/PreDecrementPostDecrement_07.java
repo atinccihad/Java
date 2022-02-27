676a59d1dc0b861b003b4826f70bf57a443c50ec
Cihad Atınç <atinccihad@gmail.com>
2022-02-28 01:56:19 +0300

    Q

---
+++ Scanner/PreDecrementPostDecrement_07.java
@@ -0,0 +1,26 @@
+package day03_scanner;
+
+public class C07_PreDecrementPostDecrement {
+
+	public static void main(String[] args) {
+		
+		int sayi=10;
+		
+		sayi--;
+		
+		System.out.println(sayi);    //9
+		 
+		int a=sayi--;
+		
+		System.out.println(a);//9
+		System.out.println(sayi);//8
+		
+		System.out.println(--a);//8
+		
+		
+		
+		
+		
+	}
+
+}
