6392baeae4b2af1ef684aca6a25824fd5a9eb10b
Cihad Atınç <atinccihad@gmail.com>
2022-03-06 21:21:07 +0300

    Q

---
+++ WrapperClass/C04_If.java
@@ -0,0 +1,33 @@
+package day06_wrapper;
+
+public class C04_If {
+
+	public static void main(String[] args) {
+		
+		int a=300;
+		int b=200;
+		
+		if (a>5) {
+			System.out.println("a sayisi besden buyuk");
+		}
+		
+		if (a>b) {
+			System.out.println("a sayisi b den buyuk");
+		}
+		
+		if (b>100) {
+			System.out.println("b sayisi 100'den buyuk");
+			
+			
+				
+			}
+		}
+		
+		// basit if cumleleri bagimsiz calisir, TUM ÝF'LERÝN BODY SÝ CALÝSABÝLÝR , bazilari calisabilir
+        //                   HATTA HÝCBÝRÝNÝN body'si calismayabilir	
+        // her bir if cumlesi kendi sartina bakar, sart true ise body calisir
+		//       sart false ise body calismaz
+	
+	}
+
+ 
