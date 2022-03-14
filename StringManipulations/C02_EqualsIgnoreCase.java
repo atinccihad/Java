a5eb9276d4723341175b7aa31c8cbec1856ee5b4
Cihad Atınç <atinccihad@gmail.com>
2022-03-14 23:30:55 +0300

    Q

---
+++ StringManipulations/C02_EqualsIgnoreCase.java
@@ -0,0 +1,24 @@
+package day11_stringManipulations;
+
+public class C02_EqualsIgnoreCase {
+
+	public static void main(String[] args) {
+		// EqualsIgnoreCase iki String i karsilastirirken case sensitive olmadan kiyas yapar
+
+		String str1="ali";
+		String str2="can";
+		String str3="Ali Can";
+		String str4=str1+" "+str2;
+		
+		System.out.println(str4); // ali Can
+		System.out.println(str3==str4); // false hem degerler hem de referanslar farkli
+		System.out.println(str3.equals(str4)); // false degerler farkli
+		
+		System.out.println(str3.equalsIgnoreCase(str4)); // true icerik ayni , case sensitive degil
+		
+		
+		
+		
+	}
+
+}
