6780029821809d2c6bc87cb0bb62337d85ee7b06
Cihad Atınç <atinccihad@gmail.com>
2022-03-16 15:15:11 +0300

    Q

---
+++ StringManipulations/C10_Contains.java
@@ -0,0 +1,38 @@
+package day11_12_stringManipulations;
+
+import java.util.Scanner;
+
+public class C10_Contains {
+
+	public static void main(String[] args) {
+		// Kullanicidan email adresini girmesini isteyin,
+		// - mail @gmail.com icermiyorsa "lutfen gmail adresi giriniz",
+		// - @gmail.com ile bitiyorsa "Email adresiniz kaydedildi" ,
+		// - @gmail.com ile bitmiyorsa "Lutfen yazimi kontrol edin" yazdirin.
+		
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Lutfen gmail adresinizi giriniz");
+		
+		String email=scan.next();
+		
+		int uzunluk=email.length();
+		int index=email.lastIndexOf("@gmail.com");
+		
+		
+		if (!email.contains("@gmail.com")) {
+			
+			System.out.println("lutfen gmail adresi giriniz");
+		} else if (index==uzunluk-10) {
+			
+			System.out.println("Email adresiniz kaydedildi");
+		} else {
+            System.out.println("lutfen yazimi kontrol edin");
+		} {
+
+		}
+		
+		scan.close();
+
+	}
+
+}
