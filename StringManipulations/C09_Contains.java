8276ed3a6490a9e59a52f38a8d6782bdeca600f0
Cihad Atınç <atinccihad@gmail.com>
2022-03-16 12:29:12 +0300

    Q

---
+++ StringManipulations/C09_Contains.java
@@ -0,0 +1,15 @@
+package day11_12_stringManipulations;
+
+public class C09_Contains {
+
+	public static void main(String[] args) {
+		
+		String cumle="Calisirsaniz, java ogrenmek cok kolay";
+		
+		System.out.println(cumle.contains(" ")); // true
+		System.out.println(cumle.contains("iz, j")); // true
+		
+		System.out.println(cumle.contains("Hasan")); // false 
+	}
+
+}
