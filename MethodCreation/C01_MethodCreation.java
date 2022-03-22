56896769e6f92d5ae614f250275c368000c0b40c
Cihad Atınç <atinccihad@gmail.com>
2022-03-22 13:05:41 +0300

    Q

--- MethodCreation/C01_MethodCreation.java
+++ MethodCreation/C01_MethodCreation.java
@@ -1,4 +1,4 @@
-package day14_methodCreation;
+package day14_15_methodCreation;
 
 public class C01_MethodCreation {
 
--- MethodCreation/C02_MethodCreation.java
+++ MethodCreation/C02_MethodCreation.java
@@ -1,78 +1,72 @@
-package day15_methodCreation;
+package day14_15_methodCreation;
 
 import java.util.Scanner;
 
 public class C02_MethodCreation {
 
 	public static void main(String[] args) {
+		// Kullanicidan iki sayi isteyin
+		// sayilarin karelerini ve küplerini toplayan iki ayri method yazdiran
+		// kullaniciya üs sorun
+		// 2 yazarsa kareleri toplamini yapan method
+		// 3 yazarsa küpler toplamini yapan method calissin
+		
+		Scanner scan=new Scanner(System.in);
+		
+		System.out.println("Lutfen iki sayiyi giriniz \nilk sayidan sonra enter'a basin");
+		
+		double sayi1=scan.nextDouble();
+		double sayi2=scan.nextDouble();
+		
+		System.out.println("girilen sayilarin kareler toplamini istiyorsaniz 2 \nkupler toplamini istiyorsaniz 3'e basin");
+		int secim=scan.nextInt();
+		
+		switch(secim) {
+		
+		case 2:   kareTopla(sayi1,sayi2);
+		   break;
+		 
+		case 3:   kupTopla(sayi1,sayi2);
+		   break;
+		default:
+			System.out.println("Lutfen istenen tercihlerden birini yapin");
+		}
+		
+		//kupTopla(sayi1,sayi2);methodu olusturmak icin kupTopla gibi bir degisken olusturmamiz gerekiyordu 
+                              //methodu olusturduktan sonra ona gerek kalmadigi icin yorum satiri haline cevirdik
+		
+		
+		//kareTopla(sayi1,sayi2);  methodu olusturmak icin kareTopla gibi bir degisken olusturmamiz gerekiyordu 
+		                         //methodu olusturduktan sonra ona gerek kalmadigi icin yorum satiri haline cevirdik
 		/*
-		 *Soru2) Kullaniciya kac sayi toplamak istedigini sorun.
-		 *Kullanici 2,3 veya 4 degerini girerse,
-		 *kullanicidab bu sayilari girmesini isteyin
-		 *ve sayilarin toplamini yazdirin. 
-		 *Kullanici toplamak istedigi sayi adedini 42den buyuk girerse
-		 *"Cok sayi girdiniz, ben toplayamam" yazdirin 
+		 * Bir method'u 4 adimda olustururuz
+		 * 1- method'u javaya olusturtabilmek icin method call yapiyoruz
+		 * 2- arguman ihtiyaci var mi belirleyelim
+		 * 3- java'dan yardim larak method'u olusturalim
+		 * 4- axcess modifier ve return type'a karar ver
+		 * bundan sonra method'un yapacagi islevi gerceklestirecek kodlarimizi yazariz
 		 */
-
-	    Scanner scan=new Scanner(System.in);
-	    
-	    System.out.println("Lutfen kac sayi toplamak istediginizi girin 2,3 veya 4 den birini secin");
-	    
-	    int tercih=scan.nextInt();
-	    if(tercih>4) {
-	    	tercih=5;
-	    }
-	    switch(tercih) {
-	    
-	    case 2:
-	    	ikisayiTopla();
-	    	break;
-	    case 3:
-	    	ucsayiTopla();
-	    	break;
-	    case 4:
-	    	dortsayiTopla();
-	    	break;
-	    case 5:
-	    	System.out.println("Cok sayi girdiniz, ben toplayamam");
-	    	break;
-	    default:
-	    	System.out.println("Gecersiz tercih");
-	    }
-	   scan.close(); 
-	}
-
-	private static void dortsayiTopla() {
-		Scanner scan=new Scanner(System.in );	
-		System.out.println("Lutfen 4 sayi girin \nher sayidan sonra enter a basin");
-		double sayi1=scan.nextDouble();
-		double sayi2=scan.nextDouble();
-		double sayi3=scan.nextDouble();
-		double sayi4=scan.nextDouble();
 		
-		System.out.println("Girdiginiz sayilarin toplami : "+(sayi1+sayi2+sayi3+sayi4));
-		scan.close();
-	} 
-
-	private static void ucsayiTopla() {
-		Scanner scan=new Scanner(System.in );
-		System.out.println("Lutfen 3 sayi girin \nher sayidan sonra enter a basin");
-		double sayi1=scan.nextDouble();
-		double sayi2=scan.nextDouble();
-		double sayi3=scan.nextDouble();
-		
-		System.out.println("Girdiginiz sayilarin toplami : "+(sayi1+sayi2+sayi3));
 		scan.close();
 	}
 
-	private static void ikisayiTopla() {
-		Scanner scan=new Scanner(System.in );
-		System.out.println("Lutfen 2 sayi girin \nher sayidan sonra enter a basin");
-		double sayi1=scan.nextDouble();
-		double sayi2=scan.nextDouble();
+	public static void kupTopla(double sayi1, double sayi2) {
 		
-		System.out.println("Girdiginiz sayilarin toplami : "+(sayi1+sayi2));
-		scan.close();
+		double kuplerToplami=sayi1*sayi1*sayi1+sayi2*sayi2*sayi2;
+		System.out.println("Girilen sayilarin kuplerinin toplami= "+kuplerToplami);
+		
+		
+	}
+
+	public static void kareTopla(double sayi3, double sayi4) {
+		// bizim arguman isimlerimizle parametre isimlerimiz ayni olmak zorunda degil
+		// java isimlere degil degerlere bakar
+		// bu bir zorunluluk degil imkandir, ýstedýgýmýzý tercýh edebiliriz
+		
+		double karelerToplami=sayi3*sayi3+sayi4*sayi4;
+		System.out.println("Girilen sayilarin kareleri toplami= "+karelerToplami);
+		
+		
 	}
 
 }
