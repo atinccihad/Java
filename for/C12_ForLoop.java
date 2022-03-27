ce1703ec5ddfaa190bed857f756abcaafabc76f2
Cihad Atınç <atinccihad@gmail.com>
2022-03-27 11:54:35 +0300

    Q

---
+++ for/C12_ForLoop.java
@@ -0,0 +1,27 @@
+package day16_17_forLoops;
+
+import java.util.Scanner;
+
+public class C12_ForLoop {
+
+	public static void main(String[] args) {
+		// Soru 11 ) Interview Question Kullanicidan 10'dan kucuk bir tamsayi isteyin
+		//           ve girilen sayinin faktoryel'ini bulun. (5!=5*4*3*2*1)  
+		
+		Scanner scan=new Scanner(System.in);
+		System.out.print("Faktoriyel hesaplamak icin 10'dan kucuk bir tam sayi giriniz");
+		int sayi=scan.nextInt();
+		
+		
+		System.out.print(sayi +"!=");
+		int faktoryel=1;
+		
+		for (int i = sayi;  i > 1   ;   i--) {
+          		faktoryel*=i;
+          		System.out.print(i+"*");
+		}
+		System.out.print("1="+faktoryel);
+		scan.close();
+	}
+
+}
