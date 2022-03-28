f373b2b15c5c05bd573e09cd06cabb73bd33d3b0
Cihad Atınç <atinccihad@gmail.com>
2022-03-28 19:14:15 +0300

    Q

---
+++ DoWhileLoop/C01_WhileLoop.java
@@ -0,0 +1,30 @@
+package day18_19_whileLoop_doWhileLoop;
+
+import java.util.Scanner;
+
+public class C01_WhileLoop {
+
+	public static void main(String[] args) {
+		// Soru 6) Kullanicidab bir sayi alin ve bu sayiyi tam bolen sayilari  
+		//  ve toplam kac tane olduklarini ekranda yazdirin
+		Scanner scan=new Scanner(System.in);
+		System.out.println("lutfen bolenlerini bulmak icin pozitif bir tam sayi giriniz");
+		int sayi=scan.nextInt();
+		
+		int sayac=0;
+		int bolen=1;
+		
+		while (bolen<=sayi) {
+			if(sayi%bolen==0) {
+				System.out.print(bolen+" ");
+                sayac++;		
+			}
+			bolen++;
+		}
+		System.out.println("");
+		System.out.println(sayi+" sayisinin "+sayac+" adet tam boleni vardir");
+		
+	scan.close();	
+	}
+
+}
