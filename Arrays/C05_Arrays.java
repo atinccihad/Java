bc3107452ec0212e335a8724fa3809d92a4a5600
Cihad Atınç <atinccihad@gmail.com>
2022-04-03 23:33:05 +0300

    Q

---
+++ Arrays/C05_Arrays.java
@@ -0,0 +1,29 @@
+package day20_21_22_23_scope_arrays_MDArrays;
+
+import java.util.Arrays;
+
+public class C05_Arrays {
+
+	public static void main(String[] args) {
+		// Soru 1: Verilen 3 elemanli bir array'in tum elemanlarini 
+		// bir soldaki konuma tasiyacak bir program yazin. 
+		// Ornek; array [1,2,3] ise output [2,3,1] olacak.
+  
+		
+		
+		int arr[]= {1,2,3,4,5,6,7,8,9};
+		int kova=arr[0];
+		
+		for (int i = 0; i < arr.length-1; i++) {
+			arr[i]=arr[i+1];
+		}
+		arr[arr.length-1]=kova;
+		
+		System.out.println(Arrays.toString(arr ));
+		
+		
+		
+		
+	}
+
+}
