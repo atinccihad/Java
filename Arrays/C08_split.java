31abc984ea17edede18bf7e5fd5cbb561e84ef69
Cihad Atınç <atinccihad@gmail.com>
2022-04-04 02:44:13 +0300

    Q

---
+++ Arrays/C08_split.java
@@ -0,0 +1,25 @@
+package day20_21_22_23_scope_arrays_MDArrays;
+
+import java.util.Arrays;
+
+public class C08_split {
+
+	public static void main(String[] args) {
+		// "Java ogrenmek cok guzel"
+		// cumlesininin kelimelerini ters sirada yazdirin
+		
+		String cumle="Java ogrenmek cok guzel";
+		
+		String kelimeler[]=cumle.split(" ");
+		
+		System.out.println(Arrays.toString(kelimeler)); // [Java, ogrenmek, cok, guzel]
+		
+		for (int i =kelimeler.length-1; i >=0 ; i--) {
+			
+			System.out.print(kelimeler[i]+" "); // guzel cok ogrenmek Java 
+		}
+		
+
+	}
+
+}
