b916506e5aa91eac78f82426e310362a47232737
Cihad Atınç <atinccihad@gmail.com>
2022-04-07 01:28:49 +0300

    Q

---
+++ List/C01_ArrayList.java
@@ -0,0 +1,30 @@
+package day24_25_arrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class C01_ArrayList {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		// list olusturalim
+		
+		List<String> isimList=new ArrayList<>();
+		
+		
+		List<String> isimList2=new ArrayList<String>();
+		// bazen esitligin sag tarafindaki elmas icine data turu yazilmazsa sorun cikabiliyor
+		// Ama genelde sorun cikmaz list olustururken sag tarafta data turu yazmayiz
+		
+		// List <String> isimList3 = new List<>();
+		// esitligin sag tarafinda List<>() yazamayiz
+		
+		 List <Integer> sayilar=new ArrayList<>();
+		 
+		 // list data turu olarak primitive leri kabul etmez
+		 
+		 List <Boolean> cevaplar=new ArrayList<>();
+
+	}
+
+}
