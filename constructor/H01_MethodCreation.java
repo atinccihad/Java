0d95ef475bba660ecad4a0b5f3dd8309c0117ff0
Cihad Atınç <atinccihad@gmail.com>
2022-04-11 23:09:02 +0300

    Q

---
+++ constructor/H01_MethodCreation.java
@@ -0,0 +1,12 @@
+package day27_28_constructor_constructorCall_staticKeyWord;
+
+public class H01_MethodCreation {
+
+	public void rakamlarToplami(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+}
