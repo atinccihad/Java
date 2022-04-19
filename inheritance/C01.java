cdd23300073c300fcd2045d85345e51c1fea4fd8
Cihad Atınç <atinccihad@gmail.com>
2022-04-19 11:53:37 +0300

    Q

---
+++ inheritance/C01.java
@@ -0,0 +1,29 @@
+package day35_36_37_encapsulation_inhertance;
+
+public class C01 {
+
+	
+		private double not=49.9;
+		
+		private boolean gecerMi=false;
+
+		public void setNot(double not) {
+			this.not = not;
+		}
+
+		public boolean isGecerMi() {
+			
+			if (not>=50) {
+				gecerMi=true;
+			}
+			return gecerMi;
+		}
+
+		
+		
+		
+	
+
+	}
+
+
