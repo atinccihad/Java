875a3c035b93bd261a068563c8c21844fc1a2cfa
Cihad Atınç <atinccihad@gmail.com>
2022-04-21 18:32:35 +0300

    Q

---
+++ overriding/Animals.java
@@ -0,0 +1,21 @@
+package day38_overriding;
+
+public class Animals {
+
+	public static void main(String[] args) {
+		
+	}
+    //  Overridden(Gecersiz kilinmis)
+	public void hareket() {
+		System.out.println("Tum hayvanlar hareket eder");
+	}
+
+	public void beslenme() {
+		System.out.println("Tum hayvanlar beslenir");
+	}
+
+    public void solunum() {
+    	System.out.println("Tum hayvanlar nefes  alir");
+    }
+
+}
