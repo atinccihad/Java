e71ff2fe3b7ac12b6246768237ede4d146db431d
Cihad Atınç <atinccihad@gmail.com>
2022-04-25 13:47:30 +0300

    Q

---
+++ AbstractClasses/Muhasebe.java
@@ -0,0 +1,10 @@
+package day42_AbstractClasses;
+
+public class Muhasebe extends Personel{
+
+	public void maas() {
+		System.out.println("Sirketteki her calisanin maasi olmali. imza:Muhasebe ");
+		
+	}
+	
+}
