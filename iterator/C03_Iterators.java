77b8ca290f1d313934f2ae8f61994448f3a1576e
Cihad Atınç <atinccihad@gmail.com>
2022-04-26 13:15:14 +0300

    Q

---
+++ iterator/C03_Iterators.java
@@ -0,0 +1,41 @@
+package day44_iterator_collections;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class C03_Iterators {
+
+	public static void main(String[] args) {
+		
+List <Integer>liste=new ArrayList<>();
+		
+		liste.add(5);
+		liste.add(3);
+		liste.add(7);
+		liste.add(1);
+		liste.add(9);
+		liste.add(8);
+		liste.add(2);
+		
+		System.out.println(liste);
+		
+		@SuppressWarnings("rawtypes")
+		Iterator ite1=liste.iterator();
+		
+		ite1.next();
+		ite1.next();
+		ite1.next();
+		
+		System.out.println(ite1.next());// 1
+		
+		ite1.remove();// son dondurdugu elementi siler
+		
+		// System.out.println(ite1.remove());
+		System.out.println(liste);// [5, 3, 7, 9, 8, 2]
+		
+		
+
+	}
+
+}
