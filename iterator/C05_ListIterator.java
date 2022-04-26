d543767077d2dc9b3bd303ff01c87d6279a956c7
Cihad Atınç <atinccihad@gmail.com>
2022-04-27 01:02:44 +0300

    Q

---
+++ iterator/C05_ListIterator.java
@@ -0,0 +1,36 @@
+package day44_iterator_collections;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
+public class C05_ListIterator {
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static void main(String[] args) {
+		
+		List<String>liste=new ArrayList<>();
+		
+		liste.add("Ali");
+		liste.add("Veli");
+		liste.add("Ayse");
+		liste.add("Fatma");
+		liste.add("Emrah");
+		
+		System.out.println(liste);// [Ali, Veli, Ayse, Fatma, Emrah]
+		
+		ListIterator li1=liste.listIterator();
+		String temp="";
+		
+		while (li1.hasNext()) {
+			
+			temp=(String) li1.next();
+			li1.set(temp+"X");
+			
+		//li1.set(li1.next()+"X");  kýsa yol
+		}
+		System.out.println(liste);// [AliX, VeliX, AyseX, FatmaX, EmrahX]
+		
+	}
+
+}
