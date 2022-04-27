2d6776dace7c241143bedd0687f94e2f480b4087
Cihad Atınç <atinccihad@gmail.com>
2022-04-27 17:21:15 +0300

    Q

---
+++ Collections/C07_Queue.java
@@ -0,0 +1,37 @@
+package day45_46_Collections;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class C07_Queue {
+
+	public static void main(String[] args) {
+		// Gelen sona gelir ayrilan bastan ayrilir
+		// FirstÝnFirstOut ==> FIFO
+		
+		Queue<String> kuyruk=new LinkedList<>();
+		
+		kuyruk.add("Lutfullah");
+		kuyruk.add("Mustafa");
+		kuyruk.add("Ridvan");
+		
+		System.out.println(kuyruk);// [Lutfullah, Mustafa, Ridvan]
+
+		kuyruk.remove();
+		System.out.println(kuyruk);//[Mustafa, Ridvan]
+		
+		kuyruk.remove();
+		System.out.println(kuyruk);// [Ridvan]
+		
+		kuyruk.add("Ridvan");
+		System.out.println(kuyruk);// [Ridvan, Ridvan]
+		
+		kuyruk.add(null);
+		kuyruk.add(null);
+		kuyruk.add(null);
+		System.out.println(kuyruk);// [Ridvan, Ridvan, null, null, null] 
+		
+
+	}
+
+}
