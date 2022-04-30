e92c3a0892af8a1e5e766c4adeb090314c9f078b
Cihad Atınç <atinccihad@gmail.com>
2022-05-01 00:33:32 +0300

    Q

---
+++ Scanner/Q_10.java
@@ -0,0 +1,22 @@
+package Day_02_Scanner;
+
+import java.util.Scanner;
+
+public class Q_10 {
+	// Write a Java program to convert temperature from Fahrenheit to Celsius degree.
+
+	// formula
+
+	    // c = (f-32)*5/9
+
+
+	    public static void main(String[] args) {
+	        @SuppressWarnings("resource")
+			Scanner scan = new Scanner(System.in);
+	        System.out.println("fahrenayt değeri giriniz : ");
+
+	        int f = scan.nextInt();
+	        System.out.println("girdiginiz fahrenayt değerinin santigrad derecesi : " + (f - 32) * 5 / 9);
+
+	    }
+}
