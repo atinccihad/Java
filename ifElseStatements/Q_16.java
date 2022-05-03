984535d64bf63f6b1b8da0468c2f7838a2780925
Cihad Atınç <atinccihad@gmail.com>
2022-05-03 13:38:50 +0300

    Q

---
+++ ifElseStatements/Q_16.java
@@ -0,0 +1,26 @@
+package Day_03_IfElseStatements;
+
+import java.util.Scanner;
+
+public class Q_16 {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		
+		
+		/* Günün sorusu: if statement
+	      katsayýlarý verilen 2 bilinmeyenli denklemin kökleriniz bulunuz.
+	        ax^2 + bx + c;
+	        Çözüm adýmlarý
+	        kullanýcýdan a,b,c yi okutun.
+	        Delta = bb - 4ac
+	        Delta > 0 => 2 tane kök vardýr x1=(-b + kök(d)) / (2a)  , x2= (-b - kök(d)) / (2*a)
+	        Delta = 0 => 1 tane kök vardýr x = -b/2a
+	        Delta < 0 ise kök yoktur.
+	   */
+		@SuppressWarnings("unused")
+		Scanner scan=new Scanner(System.in);
+
+	}
+
+}
