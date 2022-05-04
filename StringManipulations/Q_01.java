bbc8df669154959c4d2786a0f88a6a48437fc4d7
Cihad Atınç <atinccihad@gmail.com>
2022-05-04 23:35:20 +0300

    Q

--- StringManipulations/Q_01.java
+++ StringManipulations/Q_01.java
@@ -1,5 +1,21 @@
 package Day_05_StringManipulation;
 
-public class Q_01 {
+import java.util.Scanner;
 
+public class Q_01 {
+	public static void main(String[] args) {
+        // TASK : Kullanıcıdan alacağınız bir stringde boşluk karakterinin olup olmadığını kontrol ediniz.
+// TASK : Kullanıcıdan alacağınız bir stringde boşluk karakterinin olup olmadığını kontrol ediniz.
+        @SuppressWarnings("resource")
+		Scanner scan = new Scanner(System.in);
+        System.out.print("Bir string giriniz = "); // Java ogren
+        String text = scan.nextLine();
+
+        boolean boslukVarMi = text.contains(" ");
+        System.out.println("boslukVarMi = " + boslukVarMi); //boslukVarMi = true
+
+        // Kullanıcıdan alacağını bir stringin bos olup olmadığını kontrol ediniz.
+        boolean bosMu = text.isEmpty();
+        System.out.println("bosMu = " + bosMu); //bosMu = false
+    }
 }
--- StringManipulations/Q_02.java
+++ StringManipulations/Q_02.java
@@ -1,5 +1,25 @@
 package Day_05_StringManipulation;
 
-public class Q_02 {
+import java.util.Scanner;
 
+public class Q_02 {
+	public static void main(String[] args) {
+	      /* TASK :
+	       Kullanıcıdan tek seferde alacağınız 2 kelimelik ad soyadı 2 ayrı kelimeye ayırınız,
+	       ad ayrı soyad ayrı sekilde ekrana yazdırınız.
+	       */
+	        @SuppressWarnings("resource")
+			Scanner oku = new Scanner(System.in);
+	        System.out.print("Lutfen adinizi ve soyadinizi giriniz = ");
+	        String adSoyad = oku.nextLine();
+
+	        // Ahmet Yılmaz    substring(başlangıç index, e kadar - dahil değil)
+
+
+	        String ad = adSoyad.substring(0, adSoyad.indexOf(" "));
+	        String Soyad = adSoyad.substring(adSoyad.indexOf(" ")+ 1);
+
+	        System.out.println("ad = " + ad);       //ad = kader
+	        System.out.println("Soyad = " + Soyad); //Soyad = Esen
+	    }
 }
