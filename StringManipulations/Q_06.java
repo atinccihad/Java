2a046f3080d51d258529852ec40f433852d3d238
Cihad Atınç <atinccihad@gmail.com>
2022-05-05 00:03:56 +0300

    Q

---
+++ StringManipulations/Q_06.java
@@ -0,0 +1,24 @@
+package Day_05_StringManipulation;
+
+import java.util.Scanner;
+
+public class Q_06 {
+	public static void main(String[] args) {
+	       /*
+	        TASK :
+	        kullanıcıdan bir e-posta adresi girmesini isteyin, ardından "hotmail" içeriyorsa, "gmail" ile değiştirin,
+	        örneğin: johnbrown@hotmail.com ==> johnbrown@gmail.com
+	        */
+
+	        @SuppressWarnings("resource")
+			Scanner scan = new Scanner(System.in);
+	        System.out.println("Lutfen bir email giriniz");
+	        String email = scan.next();
+
+	        if(email.contains("hotmail")){
+	            email = email.replace("hotmail", "gmail");
+	        }
+	        System.out.println(email);
+
+	    }
+}
