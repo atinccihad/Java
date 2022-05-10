d8f90ed5272eec618970352060acf380d6e14e3a
Cihad Atınç <atinccihad@gmail.com>
2022-05-11 01:37:52 +0300

    Q

---
+++ List/Q_04.java
@@ -0,0 +1,22 @@
+package Day_09_List;
+
+import java.util.Scanner;
+
+public class Q_04 {
+
+	@SuppressWarnings({ "resource", "unused" })
+	public static void main(String[] args) {
+		/*  Write a Java program to get a String from user as input and find the maximumCounts occurring character in that string.
+        (Ignore case sensitivity)
+        Kullanicidan aldiginiz bir string icinde en cok tekrar eden karakteri yazdiriniz
+      input : Learning java is easy
+      output: maximumCounts occurring character is : a
+       */
+       Scanner scan = new Scanner(System.in);
+       System.out.println("bir metin giriniz : ");
+       String str = scan.nextLine();//hayat cooook bayat
+       
+       
+	}
+
+}
