83c5411e6bc4470e27b2fffe5e5e347d3f878732
Cihad Atınç <atinccihad@gmail.com>
2022-05-13 00:36:17 +0300

    Q

---
+++ for/Q_15.java
@@ -0,0 +1,24 @@
+package Day_06_ForLoop;
+
+public class Q_15 {
+	 /*
+     * write a java program that calculates the average value of array elements
+     * Print elements that are greater than avarage
+     * (dizi elemanlarinin ortalama degerini hesaplayan ve
+     * ortalamadan buyuk olanlari yazdıran bir java programi yazin)
+     * input[]= {1,2,3,4,5,6,7}
+     * Output : 4
+     */
+    public static void main(String[] args) {
+        int input[] = {1, 2, 3, 4, 5, 6, 7};
+        double toplam = 0;
+        for (int each : input) {
+            toplam += each;
+        }
+        double ort = toplam / input.length;
+        System.out.println("ort = " + ort);
+        for (int each : input) {
+            if (each > ort) System.out.println("ortalamadan buyuk olanlar :" + each);
+        }
+    }
+}
