8868a64e73c003a042825dd8ae671a9e005951ad
Cihad Atınç <atinccihad@gmail.com>
2022-05-13 00:33:34 +0300

    Q

---
+++ Scanner/Q_13.java
@@ -0,0 +1,33 @@
+package Day_02_Scanner;
+
+import java.util.Scanner;
+
+public class Q_13 {
+	 /*
+     * Kulanicidan alinan bir Stringin ilk ve son harfini yine kulanicidan alinan sayi kadar
+     * return eden metod yaziniz
+     *
+     * ornek:  input            output
+     *         elma  2           eaea
+     *         army  3           ayayay
+     */
+    public static void main(String[] args) {
+        @SuppressWarnings("resource")
+		Scanner scan = new Scanner(System.in);
+        System.out.print("Lutfen bir string giriniz :");
+        String input = scan.nextLine();
+        System.out.print("Lutfen bir sayi giriniz :");
+        int sayi = scan.nextInt();
+        ilkSonHarfBirlestir(input, sayi);
+        System.out.println(ilkSonHarfBirlestir(input,sayi));
+    }
+    private static String ilkSonHarfBirlestir(String input, int sayi) {
+        String s = input.substring(0, 1) + input.substring(input.length() - 1);
+        String output = "";
+        for (int i = 1; i <=sayi;i++){ //girilen sayi kadar yazdirsin istiyoruz
+            output += s; //   output = output +s;
+        }
+        return output;
+    }
+
+}
