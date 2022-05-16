aba1483baa10ea9be42f6d193956d56ba017122a
Cihad Atınç <atinccihad@gmail.com>
2022-05-17 01:08:43 +0300

    Q

---
+++ List/Q_01_LinkedList.java
@@ -0,0 +1,31 @@
+package Day_17_LinkedList;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Q_01_LinkedList {
+	 public static void main(String[] args) {
+			
+			/*  TASK:
+			 		Node'larin değereleri; "haluk" "ipek" "harun" "irem" olan bir LinkedList olusturun
+			 		Kullanicidan bir isim alin o isim LinkedList'de varsa silin ve kullaniciya
+			    	"Bu isim LinkedList'de vardi ve silindi" diye mesaj verin
+			 		Girilen isim yoksa "Bu isim LinkedList'de yok bu yuzden silinemedi" diye mesaj verin
+			 	
+			 */
+     LinkedList<String> ll1 = new LinkedList<>(Arrays.asList("haluk", "ipek", "harun", "irem"));
+
+     @SuppressWarnings("resource")
+	Scanner scan = new Scanner(System.in);
+     System.out.print("isim giriniz : ");
+
+     String isim =scan.nextLine().toLowerCase();
+     if (ll1.remove(isim)){
+         System.out.println("Bu isim LinkedList'de vardi ve silindi");
+         System.out.println("silinmiş hali ile list : "+ll1);
+
+     }else System.out.println("Bu isim LinkedList'de yok bu yuzden silinemedi");
+
+ }
+}
