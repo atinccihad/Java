2c8c646cd9f969f0375d9ec837ff028616713ff5
Cihad Atınç <atinccihad@gmail.com>
2022-05-16 12:47:59 +0300

    Q

---
+++ for/Q_23.java
@@ -0,0 +1,17 @@
+package Day_06_ForLoop;
+
+public class Q_23 {
+	 public static void main(String[] args) {
+
+
+
+//       1
+//       2 6
+//       3 7 10
+//       4 8 11 13
+//       5 9 12 14 15
+
+
+   }
+}
+
