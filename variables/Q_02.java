9dd38f3d829ce29c57660854f9207c27f4b346ed
Cihad Atınç <atinccihad@gmail.com>
2022-05-19 01:53:10 +0300

    Q

---
+++ variables/Q_02.java
@@ -0,0 +1,48 @@
+package Day_01_Variables;
+
+public class Q_02 {
+
+	public static void main(String[] args) {
+		
+		/*
+		 *      \n (or  \r): starts from a new line : metni bir alt satirdan itibaren yazdirir
+		 *      \t: horizontal tab:metni yatayda bir tab miktari kaydirir
+		 *      \\: prints a back slash :\ (ters slash ) yazdirir
+		 *      \'prints single quote : ' tek tirnak yazdirir
+		 *      \"prints double quote : çift tirnak yazdirir.
+		 */
+		
+		 // Soru-1 : konsolda "Hello " , "World \ /"   seklinde yazdiriniz
+		
+		System.out.println(" \"Hello\" , \"World \\ /\"  ");
+		
+		//Soru-2 : Pazartesi kelimesini her harfini ayri bir satira gelecek þekilde yazdiriniz
+		
+		System.out.println("p\na\nz\na\nr\nt\ne\ns\ni\n");
+		
+        //Soru-3 :"Java" ile hayat cok 'afilli' yazdiriniz
+		
+		System.out.println("\"Java\" ile hayat cok \'afilli\'");
+		
+		//Soru-4: "Basari" gayrete 
+		//'asiktir' yazdiriniz sonraki "TechProEd ile java cok kolay..." ifadesi
+		// 3satir sonra ve satir basindan 1 tab ileride yazilsin
+		
+		System.out.println(" \"Basari\" gayrete \n \'asiktir\'  \n \n \n \t\"TechProEd ile java cok kolay...\"");
+		
+		
+		System.out.println(" \"Basari\" gayrete \n \'asiktir\' \n\n\n\t\"TechProEd ile java cok kolay...\" ");
+				
+			   
+			
+	
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
